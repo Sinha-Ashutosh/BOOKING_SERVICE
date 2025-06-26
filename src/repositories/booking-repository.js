f97c015ac43cbe6fd787e0729e39107b0967af43
Ashutosh Sinha <Ashutosh-Sinha@outlook.com>
2025-06-26 18:14:08 +0530

    Completed create Booking

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 
 const { Booking } = require('../models/index');
 const { AppError, ValidationError } = require('../utils/errors/index');
+const { where } = require('sequelize');
 class BookingRepository {
     async create(data) {
         try {
@@ -15,6 +16,23 @@ class BookingRepository {
                 'RepositoryError',
                 'Cannot create Booking',
             'There was some issue creating the booking, please try again later',
+            StatusCodes.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    async update(bookingId, data) {
+        try {
+           const booking = await Booking.findByPk(bookingId);
+           if(data.status) {
+            booking.status = data.status;
+           }
+           await booking.save();
+            return booking;
+        } catch (error) {
+            throw new AppError(
+                'RepositoryError',
+                'Cannot update Booking',
+            'There was some issue updating the booking, please try again later',
             StatusCodes.INTERNAL_SERVER_ERROR);
         }
     }
